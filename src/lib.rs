e1cd976b32a5fb092ab54a73e70a0ef9af1889eb
Thiago Pontes <email@thiago.me>
2015-01-15 11:41:04 -0200

    make it compatible with rust alpha

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,17 @@
-#![feature(phase)]
 extern crate regex;
 
-#[phase(plugin)]
-extern crate regex_macros;
-
 use std::collections::HashMap;
+use regex::Regex;
 
 mod markov_chain;
 
 #[test]
 fn it_works() {
     let text = "\nola\nmeu nome é thiago\nyeah!";
-    let re = regex!(r"\n");
+    let re = Regex::new(r"\n");
     let returned = re.replace_all(text, ".");
     println!("{}", returned);
-    let seps = regex!("([.!?;:])");
+    let seps = Regex::new("([.!?;:])");
     let mut splits: Vec<&str> = seps.split(returned.as_slice()).collect();
     println!("\n");
     println!("{}", splits.len());
--- src/markov_chain.rs
+++ src/markov_chain.rs
@@ -1,16 +1,16 @@
 
 use std::rand::random;
 use std::collections::HashMap;
-use regex;
+use regex::Regex;
 
-pub struct MarkovChainer<'a> {
-    order: uint,
-    beginnings: Vec<Vec<&'a str>>,
-    freq: HashMap<(&'a str, &'a str), Vec<&'a str>>,
+pub struct MarkovChainer {
+    order: usize,
+    beginnings: Vec<Vec<String>>,
+    freq: HashMap<(String, String), Vec<String>>,
 }
 
-impl<'a> MarkovChainer<'a> {
-    pub fn new(order: uint) -> MarkovChainer<'a> {
+impl MarkovChainer {
+    pub fn new(order: usize) -> MarkovChainer {
         MarkovChainer {
             order: order,
             beginnings: vec!(vec!()),
@@ -18,8 +18,8 @@ impl<'a> MarkovChainer<'a> {
         }
     }
 
-    pub fn add_sentence(&mut self, string: &'a str, terminator: &'a str) {
-        let mut words: Vec<&str> = string.split(' ').collect();
+    pub fn add_sentence(&mut self, string: String, terminator: String) {
+        let mut words: Vec<String> = string.split(' ').collect::<Vec<&str>>().iter().map(|s| s.to_string()).collect();
         if words.len() > self.order {
             words.push(terminator);
             let mut vec = vec!();
@@ -27,9 +27,9 @@ impl<'a> MarkovChainer<'a> {
             self.beginnings.push(vec);
         }
 
-        let mut buf: Vec<&str> = vec!();
+        let mut buf: Vec<String> = vec!();
         for word in words.iter() {
-            buf.push(word.clone());
+            buf.push(word.to_string());
             let size = buf.len();
             if size == self.order + 1 {
                 let key = (buf[0].clone(), buf[size - 2].clone());
@@ -43,9 +43,29 @@ impl<'a> MarkovChainer<'a> {
                 continue
             }
         }
-        
+
     }
 
+    pub fn add_text(&mut self, text: String) {
+        let re = Regex::new(r"\n").unwrap();
+        let without_newline = re.replace_all(text.as_slice(), ".");
+
+        let seps = Regex::new("([.!?;:])").unwrap();
+        let mut pieces = seps.split(without_newline.as_slice());
+
+        let mut sentence = "".to_string();
+        for piece in pieces {
+            if piece != "" {
+                sentence = match seps.find(piece) {
+                    Some(_) => {
+                        self.add_sentence(sentence, piece.to_string());
+                        "".to_string()
+                    },
+                    None => piece.to_string(),
+                };
+            }
+        }
+    }
 }
 
 
